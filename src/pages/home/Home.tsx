c844e7301a454f292ca5488948c3e8a75f9a5e54
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-06-07 06:22:01 +0600

    server url change

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -28,7 +28,6 @@ export default function App() {
                     <Header />
                     <DashboardItems />
                     <GrowthAnalytics />
-                    {/* <ApexChart /> */}
                     <Disbursement />
                     <UsdtWallet />
                     <Documentation />
