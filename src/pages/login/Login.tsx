12f2d7be3ed2a751eb0882771f4b65d799453e40
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-06-11 01:00:22 +0600

    done

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -65,12 +65,12 @@ export default function Login() {
             <label
               className="block mb-2 text-sm font-medium text-gray-900"
             >
-              Your email
+              Your username
             </label>
             <input
               type="text"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="email"
+              placeholder="username"
               name="email"
               value={email}
               onChange={e => setEmail(e.target.value)}
