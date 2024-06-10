cadf79eaaddcf30d41291e67c01bdb1a15432a89
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-06-10 07:52:25 +0600

    update italic font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
     return (
         <footer className="text-center">
-            <p className="text-[8px]">
-                <b className="font-bold">DISCLAIMER: </b> 
+            <p className="text-[8px] italic">
+                <b className="font-bold text-[#343c6a]">DISCLAIMER: </b>
                 The information presented within this Client Terminal is furnished for educational and informational purposes only and does not constitute legally binding documentation or an offer to sell or solicitation of an offer to buy any securities, financial products, or instruments. Due to the inherently volatile nature of financial markets and the dynamic conditions affecting them, all balances, figures, performance metrics, and other data displayed herein are subject to change without notice and may not accurately reflect real-time values or the actual financial status of your account. Clients expressly acknowledge and agree that the only documents possessing legal enforceability and validity are the formal contracts executed between the Client and Axiom Strategic Capital, LLC. All other representations, whether verbal, written, or electronic—including the content of this Client Terminal—shall be considered subordinate and non-binding. By accessing this terminal, the Client affirms their understanding and acceptance of these conditions, thereby waiving any claims to the contrary based on the informational content provided herein.
             </p>
-            <p className="mt-5 text-sm font-bold">Copyright 2024. | Axiom Strategic Capital, LLC | All Rights Reserved.</p>
+            <p className="mt-5 text-sm font-bold text-[#343c6a]">Copyright 2024. | Axiom Strategic Capital, LLC | All Rights Reserved.</p>
         </footer>
     )
 }
