47acc941802a6f6d342ba24fb099a5066634d872
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-06-11 23:49:19 +0600

    login footer text

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -123,8 +123,8 @@ export default function Login() {
         <div className="px-8 w-10/12 mx-auto text-white mt-10">
           <footer className="text-center">
             <p className="text-[8px] italic">
-              <b className="font-bold">DISCLAIMER: </b>
-              The information presented within this Client Terminal is furnished for educational and informational purposes only and does not constitute legally binding documentation or an offer to sell or solicitation of an offer to buy any securities, financial products, or instruments. Due to the inherently volatile nature of financial markets and the dynamic conditions affecting them, all balances, figures, performance metrics, and other data displayed herein are subject to change without notice and may not accurately reflect real-time values or the actual financial status of your account. Clients expressly acknowledge and agree that the only documents possessing legal enforceability and validity are the formal contracts executed between the Client and Axiom Strategic Capital, LLC. All other representations, whether verbal, written, or electronic—including the content of this Client Terminal—shall be considered subordinate and non-binding. By accessing this terminal, the Client affirms their understanding and acceptance of these conditions, thereby waiving any claims to the contrary based on the informational content provided herein.
+              <b className="font-bold">TOS: </b>
+              By entering the protected VPSBLC.io Client Terminal, you acknowledge having read our disclaimer and agree to our terms of service concerning the information contained therein.
             </p>
             <p className="mt-5 text-sm font-bold">Copyright 2024. | Axiom Strategic Capital, LLC | All Rights Reserved.</p>
           </footer>
